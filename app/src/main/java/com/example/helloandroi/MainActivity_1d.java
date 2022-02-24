6d7c070ee0f51cf6e631eda33bd0472e7e2d910b
DangNhatKhuong <nhatkhuong2001@gmail.com>
2022-02-24 10:03:01 +0700

    'lab1_ver3'

--- app/src/main/java/com/example/helloandroi/MainActivity_1d.java
+++ app/src/main/java/com/example/helloandroi/MainActivity_1d.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity_1b extends AppCompatActivity {
+public class MainActivity_1d extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_1b);
+        setContentView(R.layout.activity_main_1d);
     }
 }
